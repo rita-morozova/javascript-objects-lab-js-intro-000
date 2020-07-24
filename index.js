c695b81f69e5dd95a277b12896cb76e81ff24d95
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 20:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  delete object.key 
+  return object 
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
