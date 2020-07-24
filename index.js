6715bf9f632fa3bcde23d13d1f5de6f69bdd5554
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 20:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var object = Object.assign({}, object, {[key]:value})
   return object
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return object[key] = value 
+}
